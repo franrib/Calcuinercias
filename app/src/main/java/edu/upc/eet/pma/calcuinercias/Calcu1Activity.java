84c3c40c3e6afbedb523d67badd1ae17d922f88d
franrib <franrib20@gmail.com>
2016-11-25 16:53:19 +0100

    Initial commit

---
+++ app/src/main/java/edu/upc/eet/pma/calcuinercias/Calcu1Activity.java
@@ -0,0 +1,13 @@
+package edu.upc.eet.pma.calcuinercias;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Calcu1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calcu1);
+    }
+}
