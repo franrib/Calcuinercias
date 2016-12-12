b989e076285d3afd8b2365e831fd9eb3bdf73b4e
franrib <franrib20@gmail.com>
2016-12-12 16:55:09 +0100

    Arreglem el layout amb GridLayout.

--- app/src/main/java/edu/upc/eet/pma/calcuinercias/Calcu1Activity.java
+++ app/src/main/java/edu/upc/eet/pma/calcuinercias/Calcu1Activity.java
@@ -1,7 +1,11 @@
 package edu.upc.eet.pma.calcuinercias;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatImageButton;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Calcu1Activity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class Calcu1Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calcu1);
+
+        ImageButton btn_cilindre = (ImageButton)findViewById(R.id.btn_cil);
+        ImageButton btn_cil_buit = (ImageButton)findViewById(R.id.btn_cil_buit);
+        ImageButton btn_basic = (ImageButton)findViewById(R.id.btn_basic);
+
+
+
     }
-}
+
+    }
