80ef2d30094e5b250d8e6dbaad6ceaa0e622eeff
Cerveto <cerveto13@gmail.com>
2017-01-12 17:21:49 +0100

    formula d'inercia del paralelogram preparades per la modalitat

--- app/src/main/java/edu/upc/eet/pma/calcuinercias/CalculatorActivity.java
+++ app/src/main/java/edu/upc/eet/pma/calcuinercias/CalculatorActivity.java
@@ -2,7 +2,10 @@ package edu.upc.eet.pma.calcuinercias;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -27,6 +30,48 @@ public class CalculatorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_calculator_relative);
 
         TextView titol_pagina = (TextView) findViewById(R.id.titol_pagina);
+
+
+        Button button = (Button)findViewById(R.id.btn_solve);
+        final EditText valor_densitat =(EditText)findViewById(R.id.valor_densitat);
+        final EditText valor_massa=(EditText)findViewById(R.id.valor_massa);
+        final EditText valor_ref1=(EditText)findViewById(R.id.valor_ref1);
+        final EditText valor_ref2=(EditText)findViewById(R.id.valor_ref2);
+        final EditText valor_ref3=(EditText)findViewById(R.id.valor_ref3);
+        final EditText resultat =(EditText)findViewById(R.id.resultat);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String sdensitat = valor_densitat.getText().toString();
+                String smassa = valor_massa.getText().toString();
+                //String sref1 = valor_ref1.getText().toString();
+                String sref2 = valor_ref2.getText().toString();
+                String sref3 = valor_ref3.getText().toString();
+                //float densitat= Float.parseFloat(sdensitat);
+                float massa= Float.parseFloat(smassa);
+                //float ref1= Float.parseFloat(sref1);
+                float ref2= Float.parseFloat(sref2);
+                float ref3= Float.parseFloat(sref3);
+                //densitat
+                //float massa = ref2*ref3*ref1*densitat
+                float inercia=(massa*((ref2*ref2)+(ref3*ref3)))/12000000;
+
+
+                String sinercia=String.format("%f",inercia);
+                resultat.setText(sinercia);
+
+                float
+
+
+
+            }
+        });
+
+
+        //ALTRES FORMULES
+
 
         //Selector d'unitats
         unitats_resultat = (Spinner)findViewById(R.id.spinner_resultat);
